8ad2c7b4364122a4ca6fc3ea00a38db2595bcf45
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-08-09 10:39:56 -0300

    update script

--- aula_js-09/08/script.js
+++ aula_js-09/08/script.js
@@ -1,1 +1,17 @@
 console.log("hello world!")
+
+console.log("oi")
+
+let string = "Eu sou o Ari";
+console.log(string);
+
+let comida = "Pâo";
+let nome = "Ari";
+
+let frase = `A comida favorita de ${nome} é ${comida}`;
+console.log(frase);
+
+let idade = 32;
+console.log(typeof idade);
+console.log(idade);
+
