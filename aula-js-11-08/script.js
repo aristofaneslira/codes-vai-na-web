64623ff6bbb5cac852db1e2a8eb1682a0b5f8c76
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-08-11 10:42:54 -0300

    aulaJavaScript

---
+++ aula-js-11-08/script.js
@@ -0,0 +1,34 @@
+var cor = "azul";
+
+cor = "pink"
+
+console.log(cor);
+
+let nome = "Maria";
+nome = "Grazy";
+nome = "Murilo"
+console.log(nome);
+
+const maiorDeIdade = true;
+console.log(typeof maiorDeIdade);
+
+let turmaT1 = "Maravilhosa";
+console.log(turmaT1);
+
+const nomeAluno = "Joao";
+let comidaFavorita = "Lasanha";
+console.log(`Meu aluno ${nomeAluno} gosta de ${comidaFavorita}`);
+
+let escola;
+console.log(escola);
+
+let teste = "cem" / 2;
+console.log(teste);
+
+let brasileiro = false;
+
+if (brasileiro) {
+    console.log("Que legal, pegue na minha mao!");
+} else {
+    console.log("nao falo sua lingua!!");
+}
