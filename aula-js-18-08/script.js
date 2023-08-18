0229ffd4866d0bc662b3c144bb37202e4b750292
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-08-18 10:43:33 -0300

    aula 18-08

---
+++ aula-js-18-08/script.js
@@ -0,0 +1,31 @@
+let numero = 1;
+
+console.log(numero + 1);
+console.log(numero + 2);
+
+let saldo = 100;
+saldo = 150;
+
+saldo += 150;
+saldo = saldo + 150;
+console.log(saldo);
+
+console.log(saldo /= 2);
+
+for(let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+let cont = 0;
+
+while (cont < 10) {
+    cont++;
+    console.log(cont);
+}
+
+let n = 0;
+
+do {
+    console.log(n);
+    n++;
+} while (n < 10);
