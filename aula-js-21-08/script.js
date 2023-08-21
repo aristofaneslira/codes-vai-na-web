d522c362869026b00ddce0f501a35940610c40d0
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-08-21 10:33:56 -0300

    aula sobre array

---
+++ aula-js-21-08/script.js
@@ -0,0 +1,15 @@
+let nomes = ["Maria", "Luiz", "Dafne", "Débora"];
+console.log(nomes); 
+
+console.log(nomes[0]);
+console.log(nomes[2][2]);
+
+console.log(nomes.length);
+
+let compras = ["feijao", "arroz", "macarrão", "café", "ovos"];
+console.log(compras);
+
+compras.push("pão");
+compras.unshift("miojo");
+compras.pop();
+compras.shift();
