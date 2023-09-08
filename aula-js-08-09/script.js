8c9fd1a12a17990928cb5e8606f124a1d54bb2d9
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-09-08 10:32:41 -0300

    aula 08-09

---
+++ aula-js-08-09/script.js
@@ -0,0 +1,10 @@
+let lampadaAcesa = document.querySelector(".lampada");
+console.log(lampadaAcesa);
+
+function lampOFF() {
+    lampadaAcesa.src = "https://goias24horas.com.br/wp-content/uploads/2013/03/celg.jpg";
+}
+
+function lampON() {
+    lampadaAcesa.src = "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2018/08/08/2018-08-08T154718Z_1_LYNXMPEE771E5_RTROPTP_3_SIEMENS-OSRAM.JPG";
+}
