6db3e8d34908649ff983c2732386d172b6a1c12f
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-09-15 10:46:12 -0300

    aula-15-09

--- aula-react/vite-project/src/App.jsx
+++ aula-react/vite-project/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react"
+import Header from "./Header"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header/>
       <h1>Hello World!!</h1>
     </>
   )
---
+++ aula-react/vite-project/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+            <p>eu sou a header</p>
+        </>
+    )
+}
+
+export default Header;
