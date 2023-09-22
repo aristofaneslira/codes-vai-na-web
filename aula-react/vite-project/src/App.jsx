2db44f82364e7dec2136fb9b14a2f0fb405ee711
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-09-22 10:47:53 -0300

    aula-22-09

--- aula-react/vite-project/src/App.jsx
+++ aula-react/vite-project/src/App.jsx
@@ -4,6 +4,8 @@ import Teste from "./Components/Teste"
 
 function App() {
   const[count, setCount] = useState(0);
+  const[pedido, setPedido] = useState("");
+  const EscolheuRisole = () => setPedido("Risole");
 
   return (
     <>
@@ -12,6 +14,14 @@ function App() {
       <button onClick={() => setCount(count+1)}>click</button>
       <h1>Hello World!!</h1>
       <Teste/>
+      <h1>Bom dia</h1>
+      <h2>Confeitaria</h2>
+      <h2>Escolha seu pedido</h2>
+      <button onClick={() => setPedido("Pizza")}>Pizza</button>
+      <button onClick={() => setPedido("Lasanha")}>Lasanha</button>
+      <button onClick={() => setPedido("Coxinha")}>Coxinha</button>
+      <button onClick={EscolheuRisole}>Risole</button>
+      <h3>Voce escolheu {pedido}</h3>
     </>
   )
 }
