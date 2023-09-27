ba4b4d940cbf05040cdc37d7b25735f30bd41cd8
Aristófanes Lira <aristofanes.lira@gmail.com>
2023-09-27 10:37:56 -0300

    aula-27-09

--- aula-25-09/vite-project/src/Components/Card.jsx
+++ aula-25-09/vite-project/src/Components/Card.jsx
@@ -1,18 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Menta from "../assets/pizza1.png";
+import Cuma from "../assets/pizza2.png";
+import Bana from "../assets/pizza3.png";
+import Toca from "../assets/pizza4.png";
 
 function Card() {
+    const[pizza, setPizza] = useState("");
+    const[imagem, setImagem] = useState();
+    const[msg, setMsg] = useState("");
+
+    const laMenta = () => {
+        setPizza("La Menta");
+        setImagem(Menta);
+        setMsg("Seu pedido é:");
+    }
+
+    const laCuma = () => {
+        setPizza("La Cuma");
+        setImagem(Cuma);
+        setMsg("Seu pedido é:");
+    }
+
+    const laBana = () => {
+        setPizza("La Bana");
+        setImagem(Bana);
+        setMsg("Seu pedido é:");
+    }
+
+    const laToca = () => {
+        setPizza("La Toca");
+        setImagem(Toca);
+        setMsg("Seu pedido é:");
+    }
+
     return (
+        <main>
+            <h2>{msg}</h2>
+            <h2>{pizza}</h2>
+            <img src={imagem} alt="" />
         <section>
             <div>
                 <img src={Menta} alt="" />
                 <h4>La Menta</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 <h2>R$ 23</h2>
-                <button>comprar agora</button>
+                <button onClick={laMenta}>Comprar agora</button>
+            </div>
+
+            <div>
+                <img src={Cuma} alt="" />
+                <h4>La Cuma</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h2>R$ 23</h2>
+                <button onClick={laCuma}>Comprar agora</button>
+            </div>
+
+            <div>
+                <img src={Bana} alt="" />
+                <h4>La Bana</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h2>R$ 23</h2>
+                <button onClick={laBana}>Comprar agora</button>
+            </div>
+
+            <div>
+                <img src={Toca} alt="" />
+                <h4>La Toca</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <h2>R$ 23</h2>
+                <button onClick={laToca}>Comprar agora</button>
             </div>
 
         </section>
+        </main>
     )
 }
 
