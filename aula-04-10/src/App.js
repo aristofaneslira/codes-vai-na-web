8912b826199430a7c36571643e7f52331b037ea6
Aristófanes Lira <31486514+aristofaneslira@users.noreply.github.com>
2023-10-04 10:36:28 -0300

    Add files via upload

---
+++ aula-04-10/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import React from "react";
+import Contador from "./Contador";
+
+export default function App() {
+  return (
+    <>
+      <h1>Contador</h1>
+      <Contador />
+    </>
+  );
+}
